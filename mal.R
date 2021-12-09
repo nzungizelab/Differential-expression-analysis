cffdd68165a2371e4edba10b9495624e1d7568c9
Nzungize Lambert <nzulapa@gmailcom>
2021-12-09 17:17:27 +0200

    second data

--- mal.R
+++ mal.R
@@ -24,6 +24,16 @@ groups <- paste(expdesign1$strain2, expdesign1$stage2, sep=".")
 groups <- factor(groups) #group	vector containing the experimental group/condition for each sample(library)
 table(groups) 
 
+
+#load gene annotation
+annot <-read.delim("gene_ane_symbols1.txt", header = TRUE) # counts can be read into a data.frame
+
+# view of data loaded
+head(annot)
+dim(annot)
+colnames(annot)
+
+
 # load libraries
 library("Rcpp")
 library("NMF")
@@ -91,10 +101,9 @@ head(fit$coefficients)
 summary(fit$df.prior)
 
 #After filtering and normalization, check how samples are relate to each other 
-plotMDS(y_filtered, method="bcv", col=as.numeric(y_filtered$samples$groups))
-legend("topleft", as.character(unique(y_filtered$samples$groups)), col=1:3, pch=20)
-
-
+plotMDS(y_filtered, 
+        col=c(rep("black",9), rep("red",7), rep("green",3),rep("yellow",3),), 
+        labels= c(rep("Clinical.Asexual",9), rep("Clinical.Sexual",7), rep("Lab.Asexual",3),rep("Lab.Sexual",3)))
 
 # comparing two groups to finds out upregulated and downregulated genes 
 # comparing 2 groups we can contrast() using the method exactTest(DGEList) to obtain DEG. 
@@ -109,3 +118,533 @@ sex <- makeContrasts(Clinical.Sexual-Lab.Sexual, levels=design)
 asexual <- makeContrasts(Clinical.Asexual-Lab.Asexual, levels=design)
 
 
+## Differential Expressed in each group (using the QL F-test).
+
+# 1. Lab strain for Sexual versus Asexual stage
+Tab1 <- glmQLFTest(fit, contrast=labSexvsAsex, coef = 2)
+
+topTags(Tab1, n= 5) # table of top 5 DEG
+
+Tab1_top.gene <- topTags(Tab1)  # table of top DEG
+Tab1_top.gene
+
+Tab1_de <- decideTestsDGE(Tab1, p.value=0.05) # pvalue using Wald test method
+#pvalue=0.01(Down 0, Up 6), pvalue=0.05(Down 7, Up 21)
+
+summary(Tab1_de) 
+# Down o gene and Up 0 gene
+
+# adjust p-values and assign the result to our table
+#we consider a fraction of 10% false positives acceptable, therefore all genes with an adjusted p value below 10% = 0.1 as significant. 
+Tab1$table$padj <- p.adjust(Tab1$table$PValue, method="BH")
+
+sum(Tab1$table$padj < 0.05)
+# 28 gene
+
+head(Tab1$table$padj)
+
+topTags(Tab1)
+
+# How many genes look significant?
+sum(Tab1$table$PValue < 0.05)
+# 397 genes
+
+sum(Tab1$table$padj < 0.05)
+# padj < 0.05 = 28
+
+# How many genes show 2-fold enrichment?
+sum(Tab1$table$PValue < 0.05 & Tab1$table$logFC > 1)
+#99 genes
+
+sum(Tab1$table$padj < 0.05 & Tab1$table$logFC > 1)
+# padj < 0.05 = 21
+
+
+# 2. Clinical strain for Sexual versus Asexual stage
+Tab2 <- glmQLFTest(fit, contrast=clinicalSexvsAsex, coef = 2)
+
+topTags(Tab2, n= 5) # table of top 5 DEG
+
+Tab2_top.gene <- topTags(Tab2)  # table of top DEG
+Tab2_top.gene
+
+Tab2_de <- decideTestsDGE(Tab2, p.value=0.05) # pvalue using Wald test method
+#pvalue=0.01(Down 0, Up 6), pvalue=0.05(Down 7, Up 21)
+
+summary(Tab2_de) 
+# Down 195 genes and Up 32 genes
+
+# adjust p-values and assign the result to our table
+#we consider a fraction of 10% false positives acceptable, therefore all genes with an adjusted p value below 10% = 0.1 as significant. 
+Tab2$table$padj <- p.adjust(Tab2$table$PValue, method="BH")
+
+sum(Tab2$table$padj < 0.05)
+# 227 genes
+
+head(Tab2$table$padj)
+
+topTags(Tab2)
+
+# How many genes look significant?
+sum(Tab2$table$PValue < 0.05)
+# 542 genes
+
+sum(Tab2$table$padj < 0.05)
+# padj < 0.05 = 227
+
+# How many genes show 2-fold enrichment?
+sum(Tab2$table$PValue < 0.05 & Tab2$table$logFC > 1)
+# 8 genes
+
+sum(Tab2$table$padj < 0.05 & Tab2$table$logFC > 1)
+# padj < 0.05 = 8
+
+
+# 3. At Sexual stage between clinical and lab strain
+Tab3 <- glmQLFTest(fit, contrast=sex, coef = 2)
+
+topTags(Tab3, n= 5) # table of top 5 DEG
+
+Tab3_top.gene <- topTags(Tab3)  # table of top DEG
+Tab3_top.gene
+
+Tab3_de <- decideTestsDGE(Tab3, p.value=0.05) # pvalue using Wald test method
+#pvalue=0.01(Down 0, Up 6), pvalue=0.05(Down 7, Up 21)
+
+summary(Tab3_de) 
+# Down 0 genes and Up 0 genes
+
+# adjust p-values and assign the result to our table
+#we consider a fraction of 10% false positives acceptable, therefore all genes with an adjusted p value below 10% = 0.1 as significant. 
+Tab3$table$padj <- p.adjust(Tab3$table$PValue, method="BH")
+
+sum(Tab3$table$padj < 0.05)
+# 0 genes
+
+head(Tab3$table$padj)
+
+topTags(Tab3)
+
+# How many genes look significant?
+sum(Tab3$table$PValue < 0.05)
+# 30 genes
+
+sum(Tab3$table$padj < 0.05)
+# padj < 0.05 = 0 gene
+
+# How many genes show 2-fold enrichment?
+sum(Tab3$table$PValue < 0.05 & Tab3$table$logFC > 1)
+# 11 genes
+
+sum(Tab3$table$padj < 0.05 & Tab3$table$logFC > 1)
+# padj < 0.05 = 0 gene
+
+
+# 4. At Asexual stage between clinical and lab strain
+Tab4 <- glmQLFTest(fit, contrast=asexual, coef = 2)
+
+topTags(Tab4, n= 5) # table of top 5 DEG
+
+Tab4_top.gene <- topTags(Tab4)  # table of top DEG
+Tab4_top.gene
+
+Tab4_de <- decideTestsDGE(Tab4, p.value=0.05) # pvalue using Wald test method
+#pvalue=0.01(Down 0, Up 6), pvalue=0.05(Down 7, Up 21)
+
+summary(Tab4_de) 
+# Down 2 genes and Up 0 genes
+
+# adjust p-values and assign the result to our table
+#we consider a fraction of 10% false positives acceptable, therefore all genes with an adjusted p value below 10% = 0.1 as significant. 
+Tab4$table$padj <- p.adjust(Tab4$table$PValue, method="BH")
+
+sum(Tab4$table$padj < 0.05)
+# 2 genes
+
+head(Tab4$table$padj)
+
+topTags(Tab4)
+
+# How many genes look significant?
+sum(Tab4$table$PValue < 0.05)
+# 50 genes
+
+sum(Tab4$table$padj < 0.05)
+# padj < 0.05 =2 gene
+
+# How many genes show 2-fold enrichment?
+sum(Tab4$table$PValue < 0.05 & Tab4$table$logFC > 1)
+# 3 genes
+
+sum(Tab4$table$padj < 0.05 & Tab4$table$logFC > 1)
+# padj < 0.05 = 0 gene
+
+
+#Volcano plot for Tab1 
+
+#change into data frame
+Tab1
+
+str(Tab1)
+
+Tab1 <- as.data.frame(Tab1)
+
+#Tab1$Gene_Name
+
+head(Tab1)
+
+dim(Tab1)
+#[1] 4681    5
+
+
+#name column header for first column as "GeneID"
+library(dplyr)
+library(tibble)
+
+Tab1 <- Tab1 %>% rownames_to_column(var = "GeneID") #Assign a variable to first column
+
+head(Tab1)
+
+
+#merge two tables (counts and gene annotation table keytype:Geneid)
+
+head(Tab1) #table 1
+dim(Tab1)
+head(annot) #table 2
+dim(annot)
+
+Tab1 <- merge(Tab1,annot, by="GeneID")
+head(Tab1, n=10)
+dim(Tab1)
+
+
+#volvano plot
+#Volcano plots represent a useful way to visualize the results of differential expression analyses.
+
+png(paste0(outpath,"volcano_Tab1.png"))
+EnhancedVolcano(Tab1,
+                #lab = rownames(Tab1),
+                lab = Tab1$GeneName,
+                x = "logFC",
+                y = "padj",
+                #y = "PValue",
+                #xlim=c(-8, 8),
+                ylim=c(0, -log10(10e-6)),
+                xlab = bquote(~Log[2]~ 'fold change'),
+                #ylab = bquote(~Log[10]~ italic('PValue')),
+                ylab = bquote(-~Log[10]~italic('padj')),
+                title = 'Lab strain Sexual vs Asexual',
+                #selectLab = c('LRP2','TNMD','AFF3','CCDC124'), # lab a set of genes pass FCcutoff and pCutoff thresholds
+                
+                #labFace = 'bold', #draw labels in bold
+                #boxedLabels = TRUE, #draw labels in boxes
+                pCutoff = 50e-3, #Cut-off for statistical significance. (horizontal line drawn at -log10(pCutoff),by default = 0.05) also can be (1.3,  10e-6, 0.0001, 10e-2)
+                #pLabellingCutoff = pCutoff, #Labelling cut-off for statistical significance. by default = pCutoff
+                FCcutoff = 0.5, #Cut-off for absolute log2 fold-change. Vertical lines drawn at the negative/positive values of FCCutoff. by deafault=2.0 also can be (1.3, 1.5,  1.7)
+                col = c("grey30", "forestgreen", "royalblue", "red2"),
+                #pointSize = 2,
+                labSize = 6, #gene label size
+                colCustom = NULL,
+                #colAlpha = 1,
+                #legendLabSize = 15,
+                legendPosition = 'bottom',
+                legendIconSize = 5.0,
+                #drawConnectors = TRUE,
+                #widthConnectors = 0.4,
+                #cutoffLineType = "longdash",
+                #cutoffLineCol = "black",
+                #cutoffLineWidth = 0.4,
+                #gridlines.major = TRUE,
+                #gridlines.minor = TRUE
+                #colConnectors = "grey50",
+                gridlines.major = FALSE,
+                gridlines.minor = FALSE,
+                border = 'partial',
+                #borderWidth = 0.5,
+                #borderColour = 'black'
+                #shape = c(1, 4, 23, 25), #each type of gene expressed with different shape
+                #drawConnectors = TRUE,
+                #colConnectors = 
+                widthConnectors = 0.75,
+                #typeConnectors =  # can be "open" or "closed",
+                #endsConnectors =   # can be ("last", "first", "both")
+                #lengthConnectors = (default = unit(0.01, 'npc'))
+                subtitle = NULL,
+                #subtitle = "XXXX",
+                #caption = paste0("total = ", nrow(toptable), " variables"),
+                caption = NULL,
+                
+)
+dev.off()
+
+
+
+#Volcano plot for Tab2 
+
+#change into data frame
+Tab2
+
+str(Tab2)
+
+Tab2 <- as.data.frame(Tab2)
+
+head(Tab2)
+
+dim(Tab2)
+
+
+#name column header for first column as "GeneID"
+Tab2 <- Tab2 %>% rownames_to_column(var = "GeneID") #Assign a variable to first column
+
+head(Tab2)
+
+
+#merge two tables (counts and gene annotation table keytype:Geneid)
+head(Tab2) #table 1
+dim(Tab2)
+head(annot) #table 2
+dim(annot)
+
+Tab2 <- merge(Tab2,annot, by="GeneID")
+head(Tab2, n=10)
+dim(Tab2)
+
+
+#volvano plot
+#Volcano plots represent a useful way to visualize the results of differential expression analyses.
+
+png(paste0(outpath,"volcano_Tab2.png"))
+EnhancedVolcano(Tab2,
+                #lab = rownames(Tab2),
+                lab = Tab2$GeneName,
+                x = "logFC",
+                y = "padj",
+                #y = "PValue",
+                #xlim=c(-8, 8),
+                ylim=c(0, -log10(10e-6)),
+                xlab = bquote(~Log[2]~ 'fold change'),
+                #ylab = bquote(~Log[10]~ italic('PValue')),
+                ylab = bquote(-~Log[10]~italic('padj')),
+                title = 'Clinical strain Sexual vs Asexual',
+                #selectLab = c('LRP2','TNMD','AFF3','CCDC124'), # lab a set of genes pass FCcutoff and pCutoff thresholds
+                
+                #labFace = 'bold', #draw labels in bold
+                #boxedLabels = TRUE, #draw labels in boxes
+                pCutoff = 50e-3, #Cut-off for statistical significance. (horizontal line drawn at -log10(pCutoff),by default = 0.05) also can be (1.3,  10e-6, 0.0001, 10e-2)
+                #pLabellingCutoff = pCutoff, #Labelling cut-off for statistical significance. by default = pCutoff
+                FCcutoff = 0.5, #Cut-off for absolute log2 fold-change. Vertical lines drawn at the negative/positive values of FCCutoff. by deafault=2.0 also can be (1.3, 1.5,  1.7)
+                col = c("grey30", "forestgreen", "royalblue", "red2"),
+                #pointSize = 2,
+                labSize = 6, #gene label size
+                colCustom = NULL,
+                #colAlpha = 1,
+                #legendLabSize = 15,
+                legendPosition = 'bottom',
+                legendIconSize = 5.0,
+                #drawConnectors = TRUE,
+                #widthConnectors = 0.4,
+                #cutoffLineType = "longdash",
+                #cutoffLineCol = "black",
+                #cutoffLineWidth = 0.4,
+                #gridlines.major = TRUE,
+                #gridlines.minor = TRUE
+                #colConnectors = "grey50",
+                gridlines.major = FALSE,
+                gridlines.minor = FALSE,
+                border = 'partial',
+                #borderWidth = 0.5,
+                #borderColour = 'black'
+                #shape = c(1, 4, 23, 25), #each type of gene expressed with different shape
+                #drawConnectors = TRUE,
+                #colConnectors = 
+                widthConnectors = 0.75,
+                #typeConnectors =  # can be "open" or "closed",
+                #endsConnectors =   # can be ("last", "first", "both")
+                #lengthConnectors = (default = unit(0.01, 'npc'))
+                subtitle = NULL,
+                #subtitle = "XXXX",
+                #caption = paste0("total = ", nrow(toptable), " variables"),
+                caption = NULL,
+                
+)
+dev.off()
+
+
+#Volcano plot for Tab3 
+
+#change into data frame
+Tab3
+
+str(Tab3)
+
+Tab3 <- as.data.frame(Tab3)
+
+head(Tab3)
+
+dim(Tab3)
+
+
+#name column header for first column as "GeneID"
+Tab3 <- Tab3 %>% rownames_to_column(var = "GeneID") #Assign a variable to first column
+
+head(Tab3)
+
+
+#merge two tables (counts and gene annotation table keytype:Geneid)
+head(Tab3) #table 1
+dim(Tab3)
+head(annot) #table 2
+dim(annot)
+
+Tab3 <- merge(Tab3,annot, by="GeneID")
+head(Tab3, n=10)
+dim(Tab3)
+
+
+#volvano plot
+#Volcano plots represent a useful way to visualize the results of differential expression analyses.
+
+png(paste0(outpath,"volcano_Tab3.png"))
+EnhancedVolcano(Tab3,
+                #lab = rownames(Tab2),
+                lab = Tab3$GeneName,
+                x = "logFC",
+                y = "padj",
+                #y = "PValue",
+                #xlim=c(-8, 8),
+                ylim=c(0, -log10(10e-6)),
+                xlab = bquote(~Log[2]~ 'fold change'),
+                #ylab = bquote(~Log[10]~ italic('PValue')),
+                ylab = bquote(-~Log[10]~italic('padj')),
+                title = 'Sexual stage between  Lab and Clinical',
+                #selectLab = c('LRP2','TNMD','AFF3','CCDC124'), # lab a set of genes pass FCcutoff and pCutoff thresholds
+                
+                #labFace = 'bold', #draw labels in bold
+                #boxedLabels = TRUE, #draw labels in boxes
+                pCutoff = 50e-3, #Cut-off for statistical significance. (horizontal line drawn at -log10(pCutoff),by default = 0.05) also can be (1.3,  10e-6, 0.0001, 10e-2)
+                #pLabellingCutoff = pCutoff, #Labelling cut-off for statistical significance. by default = pCutoff
+                FCcutoff = 0.5, #Cut-off for absolute log2 fold-change. Vertical lines drawn at the negative/positive values of FCCutoff. by deafault=2.0 also can be (1.3, 1.5,  1.7)
+                col = c("grey30", "forestgreen", "royalblue", "red2"),
+                #pointSize = 2,
+                labSize = 6, #gene label size
+                colCustom = NULL,
+                #colAlpha = 1,
+                #legendLabSize = 15,
+                legendPosition = 'bottom',
+                legendIconSize = 5.0,
+                #drawConnectors = TRUE,
+                #widthConnectors = 0.4,
+                #cutoffLineType = "longdash",
+                #cutoffLineCol = "black",
+                #cutoffLineWidth = 0.4,
+                #gridlines.major = TRUE,
+                #gridlines.minor = TRUE
+                #colConnectors = "grey50",
+                gridlines.major = FALSE,
+                gridlines.minor = FALSE,
+                border = 'partial',
+                #borderWidth = 0.5,
+                #borderColour = 'black'
+                #shape = c(1, 4, 23, 25), #each type of gene expressed with different shape
+                #drawConnectors = TRUE,
+                #colConnectors = 
+                widthConnectors = 0.75,
+                #typeConnectors =  # can be "open" or "closed",
+                #endsConnectors =   # can be ("last", "first", "both")
+                #lengthConnectors = (default = unit(0.01, 'npc'))
+                subtitle = NULL,
+                #subtitle = "XXXX",
+                #caption = paste0("total = ", nrow(toptable), " variables"),
+                caption = NULL,
+                
+)
+dev.off()
+
+
+#Volcano plot for Tab4 
+
+#change into data frame
+Tab4
+
+str(Tab4)
+
+Tab4 <- as.data.frame(Tab4)
+
+head(Tab4)
+
+dim(Tab4)
+
+#name column header for first column as "GeneID"
+Tab4 <- Tab4 %>% rownames_to_column(var = "GeneID") #Assign a variable to first column
+
+head(Tab4)
+
+#merge two tables (counts and gene annotation table keytype:Geneid)
+head(Tab4) #table 1
+dim(Tab4)
+head(annot) #table 2
+dim(annot)
+
+Tab4 <- merge(Tab4,annot, by="GeneID")
+head(Tab4, n=10)
+dim(Tab4)
+
+
+#volvano plot
+#Volcano plots represent a useful way to visualize the results of differential expression analyses.
+
+png(paste0(outpath,"volcano_Tab4.png"))
+EnhancedVolcano(Tab4,
+                #lab = rownames(Tab4),
+                lab = Tab4$GeneName,
+                x = "logFC",
+                y = "padj",
+                #y = "PValue",
+                #xlim=c(-8, 8),
+                ylim=c(0, -log10(10e-6)),
+                xlab = bquote(~Log[2]~ 'fold change'),
+                #ylab = bquote(~Log[10]~ italic('PValue')),
+                ylab = bquote(-~Log[10]~italic('padj')),
+                title = 'Sexual stage between  Lab and Clinical',
+                #selectLab = c('LRP2','TNMD','AFF3','CCDC124'), # lab a set of genes pass FCcutoff and pCutoff thresholds
+                
+                #labFace = 'bold', #draw labels in bold
+                #boxedLabels = TRUE, #draw labels in boxes
+                pCutoff = 50e-3, #Cut-off for statistical significance. (horizontal line drawn at -log10(pCutoff),by default = 0.05) also can be (1.3,  10e-6, 0.0001, 10e-2)
+                #pLabellingCutoff = pCutoff, #Labelling cut-off for statistical significance. by default = pCutoff
+                FCcutoff = 0.5, #Cut-off for absolute log2 fold-change. Vertical lines drawn at the negative/positive values of FCCutoff. by deafault=2.0 also can be (1.3, 1.5,  1.7)
+                col = c("grey30", "forestgreen", "royalblue", "red2"),
+                #pointSize = 2,
+                labSize = 6, #gene label size
+                colCustom = NULL,
+                #colAlpha = 1,
+                #legendLabSize = 15,
+                legendPosition = 'bottom',
+                legendIconSize = 5.0,
+                #drawConnectors = TRUE,
+                #widthConnectors = 0.4,
+                #cutoffLineType = "longdash",
+                #cutoffLineCol = "black",
+                #cutoffLineWidth = 0.4,
+                #gridlines.major = TRUE,
+                #gridlines.minor = TRUE
+                #colConnectors = "grey50",
+                gridlines.major = FALSE,
+                gridlines.minor = FALSE,
+                border = 'partial',
+                #borderWidth = 0.5,
+                #borderColour = 'black'
+                #shape = c(1, 4, 23, 25), #each type of gene expressed with different shape
+                #drawConnectors = TRUE,
+                #colConnectors = 
+                widthConnectors = 0.75,
+                #typeConnectors =  # can be "open" or "closed",
+                #endsConnectors =   # can be ("last", "first", "both")
+                #lengthConnectors = (default = unit(0.01, 'npc'))
+                subtitle = NULL,
+                #subtitle = "XXXX",
+                #caption = paste0("total = ", nrow(toptable), " variables"),
+                caption = NULL,
+                
+)
+dev.off()
